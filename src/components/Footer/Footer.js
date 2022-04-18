e55fed966783957bb72cad4ab782608a2ca90414
jasim-164 <jasimuddincse1703164@gmail.com>
2022-04-18 14:59:09 +0600

    Footer part added

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import "./Footer.css"
 const Footer = () => {
+    const date= new Date();
+    const year= new Date().getFullYear();
     return (
         <div>
+       
 
+        <div className="Footer text-center h-50 mt-5 bg-info"> 
+        <h4>copyright @{year}</h4>
+        </div>
 
-        <div className="Footer"> <h1>Footer</h1></div>
          
         </div>
     );
